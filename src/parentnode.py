59d12674d60dce6b2c19e175ebb6a8770b20a426
Jason Washburn <jason.washburn@gmail.com>
2025-04-15 06:39:05 -0500

    feat: Add parent node

---
+++ src/parentnode.py
@@ -0,0 +1,20 @@
+from htmlnode import HTMLNode
+
+
+class ParentNode(HTMLNode):
+    def __init__(
+        self,
+        tag: str | None,
+        children: list["HTMLNode"] | None,
+        props: dict[str, str] | None = None,
+    ):
+        super().__init__(tag=tag, children=children, props=props)
+
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("ParentNode must have a tag to convert to HTML.")
+        if len(self.children) == 0:
+            raise ValueError("ParentNode must have children to convert to HTML.")
+
+        children_html = "".join(child.to_html() for child in self.children)
+        return f"<{self.tag}{self.props_to_html()}>{children_html}</{self.tag}>"
